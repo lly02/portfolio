5ab2972f102355033e96476260c25813cfe923d6
lly02 <lly.02@outlook.com>
2023-11-01 19:54:37 +0800

    feat: basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      primary: "#fff0db",
+      secondary: "#eed9c4",
+      tertiary: "#20C20E",
+    },
     extend: {},
   },
   plugins: [],
