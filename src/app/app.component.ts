5529ed1eeb185ce3e87a11c9cf58e640eedfe60e
lly02 <lly.02@outlook.com>
2023-11-01 14:22:26 +0800

    feat: nav bar skeleton done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: '../index.html',
-  styleUrls: ['../styles.css'],
+  imports: [NavComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
