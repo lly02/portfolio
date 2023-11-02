ba5605cf9111d48c00459ee6b33f62ee8471d69d
lly02 <lly.02@outlook.com>
2023-11-02 18:31:37 +0800

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    colors: {
-      primary: "#fff0db",
-      secondary: "#eed9c4",
-      tertiary: "#20C20E",
-    },
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fff0db",
+          secondary: "#eed9c4",
+          accent: "#1F1717",
+          neutral: "#2a323c",
+          "base-100": "#1d232a",
+          "base-content": "#393646",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
+    base: false,
+    styled: true,
+    prefix: "daisy-",
+  },
+  plugins: [require("daisyui")],
 };
